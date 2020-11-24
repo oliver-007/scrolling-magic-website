1196c6a24cd7cad1b3a3657777fb21462a824666
Oliver <oliverahmed000@gmail.com>
2020-11-24 18:30:58 +0600

    sticky animated navbar added

--- scrolling.js
+++ scrolling.js
@@ -3,3 +3,8 @@ var section = document.querySelector('section');
     var value = window.scrollY;
     section.style.clipPath = "circle("+ value +"px at center)"
 })
+
+window.addEventListener("scroll", ()=>{
+    var navbar = document.querySelector("nav");
+    navbar.classList.toggle("sticky", window.scrollY > 1300);
+})
